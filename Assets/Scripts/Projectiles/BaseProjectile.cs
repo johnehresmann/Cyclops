767e962eaae5f3d912144cfd762e79a883438bd6
John Ehresmann <johnehresmann@outlook.com>
2018-07-10 22:02:22 -0700

    Setting up controller

--- Assets/Scripts/Projectiles/BaseProjectile.cs
+++ Assets/Scripts/Projectiles/BaseProjectile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BaseProjectile : MonoBehaviour {
 
     public float damage = 10f;
-    public float fireRate = 1f;;
+    public float fireRate = 1f;
     public float coolDown = 1;
 	// Update is called once per frame
 	void Update () {
