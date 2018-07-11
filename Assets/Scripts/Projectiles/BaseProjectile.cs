f0871c9032109743ab7490aeb63b6ba395a50d54
John Ehresmann <johnehresmann@outlook.com>
2018-07-10 18:19:24 -0700

    added projectile

---
+++ Assets/Scripts/Projectiles/BaseProjectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseProjectile : MonoBehaviour {
+
+    public float damage = 10f;
+    public float fireRate = 1f;;
+    public float coolDown = 1;
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
