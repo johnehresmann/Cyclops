61a17ad55866d12d0e78bcdb3fc92b91b5018285
John Ehresmann <johnehresmann@outlook.com>
2018-07-15 21:40:25 -0700

    Shooting Setup

---
+++ Assets/Scripts/MasterProjectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MasterProjectile : MonoBehaviour {
+
+	// Use this for initialization
+	
+	Rigidbody projectileRigid;
+
+	private Transform prefabTransform;
+	private Transform trans;
+
+	private EasyInputVR.Misc.FirstPersonTurnAndShoot fpsTurn;
+
+	#region Player Variables
+
+	public float shotSpeed = 40f;
+
+	#endregion
+	
+	void Start () {
+
+		
+				
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		this.transform.Translate(0,0,shotSpeed * Time.deltaTime);	
+		
+	}
+}
