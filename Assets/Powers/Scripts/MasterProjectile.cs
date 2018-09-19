a1b403737bb384fec97086785ccbe003165c8a85
John Ehresmann <johnehresmann@outlook.com>
2018-09-18 22:31:46 -0700

    Implemented basic shooting

---
+++ Assets/Powers/Scripts/MasterProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterProjectile : MonoBehaviour 
+{
+	public float projectileSpeed = 10;
+	public GameObject playerObject;
+
+	public LayerMask playerLayer = 9;
+	
+
+	void Start()
+	{
+		
+	
+	}
+		/* Camera playerCamera = playerObject.GetComponent<Camera>();
+		Transform cameraTransform = playerCamera.transform;
+		this.transform.forward += cameraTransform.forward * projectileSpeed; */
+	void Update()
+	{
+		if (Physics.Raycast(transform.position, transform.forward, 100, playerLayer.value))
+			Debug.Log("Player");
+		
+	}
+}
